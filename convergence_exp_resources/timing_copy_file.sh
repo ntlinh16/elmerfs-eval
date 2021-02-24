54cea604e12050c10f9cf09ae7965832d63838ca
ntlinh16 <ntlinh16@gmail.com>
2021-02-24 21:55:13 +0100

    feat: checksum file after copying to ensure the copy process completes sucessfully

--- convergence_exp_resources/timing_copy_file.sh
+++ convergence_exp_resources/timing_copy_file.sh
@@ -3,10 +3,20 @@
 file_path="$1"
 dest_path="$2"
 log_path="$3"
+hash="$4"
+checksum_path="$5"
 
 # save the t_start before copying
 echo $(date +"%D %T.%N") >> $log_path
 
 # save the t_start2 after finishing copying
 cp $file_path $dest_path && echo $(date +"%D %T.%N") >> $log_path
+
+cur_hash=$(sha256sum $dest_path | awk '{print $1}')
+echo $cur_hash
+
+# verify the hash
+if [ $hash == $cur_hash ]; then
+    echo "checksum OK"
+    echo "1" >> $checksum_path
 
